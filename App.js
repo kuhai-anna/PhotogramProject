b1d87b085bd345f62b648a58802c2a64b19e438e
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-06 23:06:06 +0300

    Comment the code

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>New App!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <></>
+    // <View style={styles.container}>
+    //   <Text>New App!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#F6F6F6",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#F6F6F6",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
