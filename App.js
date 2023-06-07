82671b11b5c29afe8b150216acde754cad45e0d4
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-07 04:30:05 +0300

    Add RegistrationScreen

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 // import { StatusBar } from "expo-status-bar";
 // import { StyleSheet, Text, View } from "react-native";
 
+import { RegistrationScreen } from "./src/Screens/RegistrationScreen";
+
 export default function App() {
   return (
-    <></>
+    <RegistrationScreen></RegistrationScreen>
     // <View style={styles.container}>
     //   <Text>New App!</Text>
     //   <StatusBar style="auto" />
